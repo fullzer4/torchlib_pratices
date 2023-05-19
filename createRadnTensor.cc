f879a70c77f18c346fb2d77ad6f7142c44d83d84
fullzer4 <gabrielpelizzaro@gmail.com>
2023-05-19 15:43:05 +0000

    ew

--- createRadnTensor.cc
+++ createRadnTensor.cc
@@ -1,11 +1,15 @@
 #include <torch/torch.h>
 #include <iostream>
+#include <std.h>
 
 void randTensor(){
-    torch::Tensor tensor = torch::rand({2, 3});
-    return tensor;
+
+    torch::Tensor tensor;
+    tensor = torch::rand({2, 3});
+    std::cout << tensor << std::endl;
+    
 }
 
 int main() {
-    std::cout << randTensor();
+    randTensor();
 }
